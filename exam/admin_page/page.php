b8b90c432f761af3e3cac59cacf6dfde26931772
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-04 14:42:15 +0530

    no message

--- exam/admin_page/page.php
+++ exam/admin_page/page.php
@@ -1,23 +1,27 @@
 <!DOCTYPE html>
+<?php
+include_once ("veri_admin.php");
+?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Admin's Page</title>
-    <link rel="stylesheet" href="page.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
 
   <body>
 
 
-    <div class="left">
+  <div class="left">
       <div class="dropdown-menu">
-      <button class="menu-btn">Admin's Section</button>
+        <button class="menu-btn">Admin's Section</button>
       <div class="menu-content">
-      <a class="links" href="#">Add Question</a>
+      <a class="links" href="../Add_question/AddNewQues.php">Add Question</a>
       <a class="links" href="#">Modification</a>
       <a class="links" href="#">Reports</a>
       <a class="links" href="#">OneView</a>
-      <a class="links" href="#">Logout</a>
+      <a class="links" href="set_exam/set_exam.php">Set exam</a>
+      <a class="links" href="/php/exam_logout.php">Logout</a>
 </div>
 
 </div>
