05867a06e94c016bc2a7132cbcd0709e7efd87d3
somnathmondal1998 <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-08 15:38:08 +0530

    Update veri_admin.php

--- exam/admin_page/veri_admin.php
+++ exam/admin_page/veri_admin.php
@@ -3,11 +3,7 @@
 session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
-<<<<<<< HEAD
 if($_SESSION["privileges"] !== "admin")
-=======
-if ($_SESSION["privileges"] !== "admin")
->>>>>>> 6c530246508c1c17b0c1527ee9532f2e40172a1a
 {
     header("location: http://www.americaniche.com/exam/index.php");
     exit;
