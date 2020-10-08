1ea745bc50efbaba61df43be600391c5dd3f5131
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-08 17:44:18 +0530

    Update Question.php

--- php/Question.php
+++ php/Question.php
@@ -1,33 +1,4 @@
-<?php
-session_start();
-
- //alert funtion:
- function function_alert($message) {
-  echo "<script>alert('$message');</script>";
-}
-
-include('data_base_exam.php');
-$qtype = $_POST['question_type'];
-$ques = $_POST['question'];
-$format = $_POST['format'];
-$level = $_POST['level'];
-if($qtype)
-{
-  $op1 = $_POST['option1'];
-  $op2 = $_POST['option2'];
-  $op3 = $_POST['option3'];
-  $op4 = $_POST['option4'];
-  $ans = $_POST['answer'];
-}
-$ans = 1;
-$creationdate = date("jS \of F Y");
-$createdby = $_SESSION['username'];
-$sql1 = "INSERT INTO Questions (Questiontext,Right_option,Questiontype,Format,CreatedBy,ExpertLevel) VALUES ('$ques','$ans','$qtype','$format','$createdby','$level' )";
-//$sql2 = "INSERT INTO option VALUES ( , ,)";
-
-if (mysqli_query($conn_1, $sql1))
-{
-  <html>
+<html>
    <head>
      <style>
       select {
@@ -100,8 +71,38 @@ if (mysqli_query($conn_1, $sql1))
 
     </style>
     <title>ADD Question</title>
-   </head>
-   <body>
+ </head>
+<?php
+session_start();
+
+ //alert funtion:
+ function function_alert($message) {
+  echo "<script>alert('$message');</script>";
+}
+
+include('data_base_exam.php');
+$qtype = $_POST['question_type'];
+$ques = $_POST['question'];
+$format = $_POST['format'];
+$level = $_POST['level'];
+if($qtype)
+{
+  $op1 = $_POST['option1'];
+  $op2 = $_POST['option2'];
+  $op3 = $_POST['option3'];
+  $op4 = $_POST['option4'];
+  $ans = $_POST['answer'];
+}
+$ans = 1;
+$creationdate = date("jS \of F Y");
+$createdby = $_SESSION['username'];
+$sql1 = "INSERT INTO Questions (Questiontext,Right_option,Questiontype,Format,CreatedBy,ExpertLevel) VALUES ('$ques','$ans','$qtype','$format','$createdby','$level' )";
+//$sql2 = "INSERT INTO option VALUES ( , ,)";
+
+if (mysqli_query($conn_1, $sql1))
+{
+  
+   echo "<body>
 
       <div id="heading">
        <img id="logo" src="logo.png">
@@ -192,8 +193,8 @@ if (mysqli_query($conn_1, $sql1))
             }
           }
 
-  </script>
-  </html>
+  </script>";
+  
 }
 else
 {
@@ -202,3 +203,4 @@ else
   header("location: /exam/Add_question/AddNewQues.php");
 }
 ?>
+</html>
