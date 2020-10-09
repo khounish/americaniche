33277422c851d0f793d10cc05e782f1f9fc029f0
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-09 23:55:41 +0530

    Update exam_set.php

--- php/exam_set.php
+++ php/exam_set.php
@@ -12,15 +12,15 @@
 //connection to db:
  require_once "data_base_exam.php";
 //save informition to database:
-    $sql1 = "INSERT INTO Exam_scheduler (userid,Date,Start_time,Duration,Subject,Level,Examinator) VALUES ('$user_id','$date', '$time', '$duration', '$subject','$level', '$examinator')";
+$sql1 = "INSERT INTO Exam_scheduler (userid,Date,Start_time,Duration,Subject,Level,Examinator) VALUES ('$user_id','$date', '$time', '$duration', '$subject','$level', '$examinator')";
 if(mysqli_query($conn,$sql1))
 {
-    $_SESSION["m"] = "updated";
-    header ("Location: /exam/admin_page/set_exam/set_exam.php");
+  $_SESSION["m"] = "updated";
+  header ("Location: /exam/admin_page/set_exam/set_exam.php");
 }
 else
 {
-    $_SESSION["m"] = "not updated";
-    header ("Location: /exam/admin_page/set_exam/set_exam.php");
+ $_SESSION["m"] = "not updated";
+ header ("Location: /exam/admin_page/set_exam/set_exam.php");
 }
 ?>
