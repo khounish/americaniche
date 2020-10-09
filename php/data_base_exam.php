ee4b09dac4644eb7555529e40fac3242f7bc1c58
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-09 22:55:35 +0530

    Update data_base_exam.php

--- php/data_base_exam.php
+++ php/data_base_exam.php
@@ -6,6 +6,6 @@ $conn_1 = mysqli_connect("192.168.56.101","somnath","somnath1","Core_Question_Ba
 //connection checking
 if($conn === false)
 {
-  die("ERROR:connection not successful" . mysqli_connect_error());
+  //die("ERROR:connection not successful" . mysqli_connect_error());
 }
 ?>
