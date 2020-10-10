48d84f1655fe2619b9d22c0851fd9a84ee5c222e
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-10 17:40:45 +0530

    no message

--- php/exam_set.php
+++ php/exam_set.php
@@ -2,7 +2,7 @@
 //verify authentification:
  require_once "veri_admin.php";
 //get input from form:
- $user_id = $_POST["userid"];
+ $username = $_POST["username_s"];
  $date = $_POST["date"];
  $category_id = $_POST["category_id"];
  $duration =$_POST["duration"];
@@ -11,8 +11,11 @@
  $examinator = $_SESSION['username'];
 //connection to db:
  require_once "data_base_exam.php";
+ require_once "data_base.php";
+ $sql1 = "SELECT user_id FROM userdetails WHERE username = '$username'";
+ $userid = (($conn->query($sql1))->fetch_assoc())["user_id"];
 //save informition to database:
-$sql1 = "INSERT INTO Exam_scheduler (id,user_id,Date,Duration,Categoty_id,Level,NoQues,Examinator) VALUES (NULL,'$user_id','$date','$duration','$category_id','$level','$noques','$examinator')";
+$sql1 = "INSERT INTO Exam_scheduler (id,user_id,Date,Duration,Categoty_id,Level,NoQues,Examinator) VALUES (NULL,'$userid','$date','$duration','$category_id','$level','$noques','$examinator')";
 if(mysqli_query($conn_1,$sql1))
 {
   $_SESSION["m"] = "updated";
