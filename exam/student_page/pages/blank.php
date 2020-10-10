41bbb41a9f2e439422ed3e7cb61d0702a02e2c11
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-10 18:57:05 +0530

    Update blank.php

--- exam/student_page/pages/blank.php
+++ exam/student_page/pages/blank.php
@@ -6,12 +6,16 @@ include_once ("v_s.php");
 div.a {
   text-align: center;
 }
+  p{
+    text-align:center;
+    font-size:30px;
+  }
 </style>
    <body >
 <div class=a>
  <img src="../image/logo.png">
       <h1> Welcome to Americaniche Exam Portal.</h1> </div>
-      <br><p style="color : red"><?php echo htmlspecialchars($_SESSION["s_error"]); ?>
+     <br><p style="color : red"><?php echo htmlspecialchars($_SESSION["s_error"]); ?></p>
    </body>
 
 </html>
