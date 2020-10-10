8ca5f2df310fe687596eb2d8a362d4a413858fee
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-10 10:48:07 +0530

    no message

---
+++ exam/admin_page/index.php
@@ -0,0 +1,20 @@
+<?php
+require_once "veri_admin.php";
+ ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Americaniche Exam Portal</title>
+    </head>
+    <frameset cols = "13.2%,*" border="0">
+      <frame src = "page.php" name = "menu_page" />
+      <frame src = "blank.php" name = "main_page" />
+
+      <noframes>
+          <body>Your browser does not support frames.</body>
+      </noframes>
+
+      </frameset>
+
+</html>
