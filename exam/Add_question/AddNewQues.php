cdc7c917efeb03141090a9673bb8f2ac5e1d1c41
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-10 06:45:56 +0530

    Update AddNewQues.php

--- exam/Add_question/AddNewQues.php
+++ exam/Add_question/AddNewQues.php
@@ -86,7 +86,7 @@ require_once "veri.php";
      <div id="Quest"><h5>Click on preview to see the the question here before adding to database</h5>
       <?php
        include('data_base_exam.php');
-       $sql1 = "SELECT * FROM Questions where Id=(SELECT LAST_INSERT_ID())";
+       $sql1 = "SELECT * FROM Questions where order by Question_id DESC LIMIT 1";
        if($result = mysqli_query($conn_1,$sql1))
        {
          if(mysqli_num_rows($result)>0)
