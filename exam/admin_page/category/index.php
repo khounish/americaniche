de16ed254b7bfac9414d401d9cc178c13d639465
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-10 16:18:29 +0530

    cosmetic changes to category

--- exam/admin_page/category/index.php
+++ exam/admin_page/category/index.php
@@ -9,17 +9,16 @@ require_once "veri_admin.php";
            }
 
      body{
-       background-color: rgb(242, 242, 242);
+       background-color: white;
        font-size: 30px;
-
          }
 
-     h3{
+    h3{
       position: relative;
       left :45px;
       top: 20px;
       font-size: 50px;
-       }
+    }
 
 	   h4{
 	      margin-top:-5px;
@@ -52,50 +51,49 @@ require_once "veri_admin.php";
              left:10px;
            }
         #heading{
-          background-color: rgba(3, 13, 147, 0.6);
+          background-color: #d8d9da;
           height:85px;
-          color: white;
                 }
         #subheading{
           margin-top: 0px;
-          background-color: rgb(186, 94, 94, 0.4);
+          background-color: #7c89c0;
           height:52px;
+          color:white;
                    }
         #Quest{
                    float: right;
-                   height: 100%;
+                   height: 55%;
                    width: 45%;
-                   background-color: white;
+
                    font-size: 30px;
                    text-align: center;
-
+                   margin-top:-20px;
+		   right:15px;
+		   border-style: outset;
+		   border-color: #7c89c0;
+		   border-width: 10px;
                  }
-
+      a{
+        color:orange;
+      }
 
   </style>
   <title>ADD Question</title>
  </head>
  <body>
-    <!--Heading division-->
+
     <div id="heading">
-     <img id="logo" src="logo.png">
+
      <h3>Americaniche Academy</h3>
     </div>
+	  <h4 id="subheading"><i>ADD NEW CATEGORY<i></h4>
 
+      <form name="CATEGORY" action="php/category.php" method="post" >
 
-	  <h4 id="subheading"><i>ADD NEW CATEGORY ID<i></h4>
-
-
-        <form name="CATEGORY" action="php/category.php" method="post" >
-
-         <p>Enter the topic of the question:<input type="text" name="topic" required>
-         <p>Enter the subtopic of the question:<input type="text" name="subtopic" required>
-         <p>Enter the keyword of the question:<input type="text" name="keyword" required><br><br>
-         <button class="button" type="submit">Add Category</button>
-         <br><?php echo htmlspecialchars($_SESSION["m_c"]); ?>
-
-        </form>
-
- </body>
-
+       <p>Enter the topic of the question:<input type="text" name="topic" required>
+       <p>Enter the subtopic of the question:<input type="text" name="subtopic" required>
+       <p>Enter the keyword of the question:<input type="text" name="keyword" required><br><br>
+       <button class="button" type="submit">Add Category</button>
+       <br><?php echo htmlspecialchars($_SESSION["m_c"]); ?>
+     </form>
 </html>
