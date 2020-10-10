4c76c273c1c07652fc25f77d44c93e0371a7da71
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-10 14:06:26 +0530

    no message

--- exam/student_page/pages/v_s.php
+++ exam/student_page/pages/v_s.php
@@ -3,9 +3,10 @@
 session_start();
 
 // Check if the user is logged in, if not then redirect him to login page
-if($_SESSION["privileges"] !== "student")
+if($_SESSION["s_privileges"] !== "student")
 {
     header("location: http://www.americaniche.com/exam/");
     exit;
+    session_destroy();
 }
 ?>
--- exam/student_page/v_s.php
+++ exam/student_page/v_s.php
@@ -5,7 +5,8 @@ session_start();
 // Check if the user is logged in, if not then redirect him to login page
 if($_SESSION["s_privileges"] !== "student")
 {
-    header("location: http://www.americaniche.com/");
+    header("location: http://www.americaniche.com/exam/");
     exit;
+    session_destroy();
 }
 ?>
