1b4ecdde75393212758f1ac3c55b17cbaef7c6ae
somnathmondal1998 <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-19 16:26:13 +0530

    Update center.php

--- zurich_ins/main_page/center.php
+++ zurich_ins/main_page/center.php
@@ -3,19 +3,31 @@ require_once "veri.php";
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <style>
+ <style type="text/css">
 body {
-  background-image: url("/zurich_ins/images/lobby.jpg");
-  background-repeat: no-repeat;
-  background-attachment: fixed; 
-  background-size: 100% 100%;
-}
-  </style>
+    font-family:verdana,arial,sans-serif;
+    font-size:10pt;
+    margin:30px;
+    background-color:#ffcc00;
+    }
+</style>
+<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
 
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+
+</head>
+<body>
+<h1>Content</h1>
+<br>
+
+<button id="button1">Toggle 'em</button>
+<p>Hiya</p>
+<p>Such interesting text, eh?</p>
+
+<script>
+$('button').on('click',function(){
+     $( "p" ).toggle( "slow" );
+      $( "h1" ).toggle( "slow" );
+});
+</script>
   </body>
 </html>
