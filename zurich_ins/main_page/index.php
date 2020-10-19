0ab1406062cb57a05ae1d13b09a28c5e9715e1d3
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-19 15:53:58 +0530

    Update index.php

--- zurich_ins/main_page/index.php
+++ zurich_ins/main_page/index.php
@@ -19,15 +19,6 @@ require_once "veri.php";
 
    <frame src = "left.php" name = "left" scrolling="no" />
    <frame src = "center.php" name = "center"/>
-
-   <div id="webterminal">
-   <frameset rows = "*,250" border="0">
-
-   <frame src = "center.php" name = "center"/>
-   <frame src = "shellinabox.php" name = "centerdown"/>
-
-   </frameset>
-  </div>
    <frame src = "right.php" name = "right" scrolling="no" />
  
 </frameset>
