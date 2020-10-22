8d60135c216ba3f330c33b46184579737b62c642
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-22 10:15:21 +0530

    Update exam_set.php

--- php/exam_set.php
+++ php/exam_set.php
@@ -4,18 +4,27 @@
 //get input from form:
  $username = $_POST["username_s"];
  $date = $_POST["date"];
- $category_id = $_POST["category_id"];
+//$category_id = $_POST["category_id"];
+$keyword = $_POST['keyword'];
+$topic = $_POST['topic'];
+$subtopic = $_POST['subtopic'];
  $duration =$_POST["duration"];
  $level = $_POST["level"];
- $noques = $_POST["noques"];
+ //$noques = $_POST["noques"];
  $examinator = $_SESSION['username'];
 //connection to db:
  require_once "data_base_exam.php";
  require_once "data_base.php";
+$sql = "SELECT * FROM QuesCat WHERE Topic = '$topic' and Subtopic = '$subtopic' and Keywords = '$keyword'";
+if($result = mysqli_query($conn_1, $sql))
+{
+ $row = mysqli_fetch_assoc($result);
+ $category = $row['Category_id'];
+}
  $sql1 = "SELECT user_id FROM userdetails WHERE username = '$username'";
  $userid = (($conn->query($sql1))->fetch_assoc())["user_id"];
 //save informition to database:
-$sql1 = "INSERT INTO Exam_scheduler (id,user_id,Date,Duration,Category_id,Level,NoQues,Examinator) VALUES (NULL,'$userid','$date','$duration','$category_id','$level','$noques','$examinator')";
+$sql1 = "INSERT INTO Exam_scheduler (id,user_id,Duration,Category_id,Level,Examinator) VALUES (NULL,'$userid','$duration','$category','$level','$examinator')";
 if(mysqli_query($conn_1,$sql1))
 {
   $_SESSION["m"] = "updated";
