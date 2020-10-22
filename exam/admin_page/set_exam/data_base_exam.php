e1bc2be7a5d8b9739f85deef3ff982853a1ca1a1
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-22 10:24:22 +0530

    adding data_base_exam file to set_exam folder

---
+++ exam/admin_page/set_exam/data_base_exam.php
@@ -0,0 +1,8 @@
+<?php
+$conn_1 = mysqli_connect("192.168.56.101","somnath","somnath1","Core_Question_Bank");
+//connection checking
+if($conn_1 === false)
+{
+  die("ERROR:connection not successful" . mysqli_connect_error());
+}
+?>
