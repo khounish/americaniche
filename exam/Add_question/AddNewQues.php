d1877ef807f647030aa262a4eca2d8da9e306ddf
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-24 08:57:52 +0530

    s

--- exam/Add_question/AddNewQues.php
+++ exam/Add_question/AddNewQues.php
@@ -119,12 +119,10 @@ tr:nth-child(even) {
            $sql2 = "SELECT distinct Topic FROM QuesCat";
             $result2 = mysqli_query($conn_1,$sql2);
           while($row2 = mysqli_fetch_assoc($result2))
-
-             {
-              $t=$row['Topic'];?>
-             <button onclick="showQuestion(<?php $t ?>)"><?php $t ?></button>";
-            <?php}
-          ?>
+          {
+            $t = $row2['Topic'];
+            echo "<button onclick="showQuestion($t)">$t</button><br>";
+          }
       <?php
        /*include('data_base_exam.php');
        $sql1 = "SELECT * FROM Questions order by Question_id DESC";
