4e236348f7fa028d708058a28f6fa1c13b87d03a
anamika-singh-1 <71555916+anamika-singh-1@users.noreply.github.com>
2020-10-24 09:36:19 +0530

    a

--- exam/Add_question/quest_bank.php
+++ exam/Add_question/quest_bank.php
@@ -1,19 +1,19 @@
 <?php
 $conn_1 = mysqli_connect("192.168.56.101","somnath","somnath1","Core_Question_Bank");
 //connection checking
-if($conn === false)
+if($conn_1 === false)
 {
   die("ERROR:connection not successful" . mysqli_connect_error());
 }
-$q = $_REQUEST['q'];
+$q = $_GET['q'];
 echo "$q";
-/*$sql = "SELECT Category_id FROM QuesCat WHERE Topic = '$q'";
+$sql = "SELECT Category_id FROM QuesCat WHERE Topic = '$q'";
+echo "$sql";
 $result = mysqli_query($conn_1,$sql);
 while($row = mysqli_fetch_assoc($result))
 {
   $p = $row['Category_id'];
   $sql1 = "SELECT * FROM Questions WHERE Category_id = '$p'";
-   echo "$sql1";
   if($result = mysqli_query($conn_1,$sql1))
   {
     if(mysqli_num_rows($result)>0)
@@ -32,5 +32,5 @@ while($row = mysqli_fetch_assoc($result))
  {
     echo "<p>Unable to make query to database</p>";
   }
-}*/
+}
  ?>
