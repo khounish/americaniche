dc814ca6a20c9b2b64266da3d6aff0c79b1a7d32
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-31 21:41:37 +0530

    t

--- page/menu.php
+++ page/menu.php
@@ -6,8 +6,7 @@ require_once "v_s.php";
 <link rel="stylesheet" type="text/css" href="css/style.css" />
  </head>
    <body bgcolor = "#ecf0f3" color=red><b>
-   <div class="login-div">
-      <div class="logo"></div><br><br>
+    <div class="logo"></div><br><br>
 
    <ul id="myUL" style="margin-top:50">
   <ul id="myUL">
