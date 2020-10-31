d611acba0e7b0b7bca753439d027462afdf05c3a
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-31 22:55:31 +0530

    s

--- page/switch.php
+++ page/switch.php
@@ -4,3 +4,11 @@
           <input type="checkbox">
 <span class="slider round"></span>
 </label>
+
+
+
+
+<div id="toggle">
+        <input type="checkbox" id="myCheckbox" onchange="toggleCheck()" name="">
+    </div>
+      <div id="a"><h3>webterminal</h3></div>
