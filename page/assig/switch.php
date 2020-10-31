4cb5782aff5312cff2c6a2ccd1b761c7e9b33d64
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-31 23:08:07 +0530

    s

--- page/assig/switch.php
+++ page/assig/switch.php
@@ -1,6 +1,6 @@
 <div><h3>webterminal</h3></div>
 
 <label class="switch" onchange="toggleCheck()">
-          <input type="checkbox" onchange="toggleCheck()">
+          <input type="checkbox" id="myCheckbox" onchange="toggleCheck()" name="">
 <span class="slider round"></span>
 </label>
