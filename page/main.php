52d1a382ea507aac4177c1d98075f5db408b228d
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-10-31 22:28:25 +0530

    f

--- page/main.php
+++ page/main.php
@@ -4,13 +4,28 @@ require_once "v_s.php";
 <!DOCTYPE html>
 <html>
 <style>
-div.a {
-  text-align: center;
+.logo {
+    background:url("/photos/logo_big.png");
+    background-size: cover;
+    width:110px;
+    height: 110px;
+    border-radius: 50%;
+    margin:0 auto;
+    box-shadow:
+    /* logo shadow */
+    0px 0px 2px blue,
+    /* offset */
+    0px 0px 0px 5px #24cfaa,
+    /*bottom right */
+    8px 8px 15px #a7aaaf,
+    /* top left */
+    -8px -8px 15px #ffffff
+    ;
 }
 </style>
    <body >
 <div class=a>
- <img src="/photos/logo.png" align=center alt="logo" width="300px" height="300px">
+<div> <div class="logo"></div><br><br></div>
       <h1> Hello! <?php echo htmlspecialchars($_SESSION["username"]); ?> Welcome to AmericaNiche Academy</h1> </div>
    </body>
 
