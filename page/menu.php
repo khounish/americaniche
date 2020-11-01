13fd0c120a33c1c146743633d9e6befb5ddd9cc6
Somnath Mondal <71555867+somnathmondal1998@users.noreply.github.com>
2020-11-01 23:30:06 +0530

    F

--- page/menu.php
+++ page/menu.php
@@ -7,10 +7,11 @@ require_once "v_s.php";
  </head>
    <body bgcolor = "#025241" color=red>
    <div id="position1"> <div class="logo"></div><br><br></div>
-   <div id="position2">
-   <br><a href="practice.php" target="main_page">Practice now</a>
+
+<div id="position2">
+<br><a href="practice.php" target="main_page">Practice now</a>
 <br>
-<p><a href="../php/logout.php" target="_top" style="color:white;">Logout</a></p><br>
+<a href="../php/logout.php" target="_top" style="color:white;">Logout</a>
 </div>
 
 
@@ -29,10 +30,6 @@ require_once "v_s.php";
       <li><a href = "/videos/day1.php" target = "main_page">Day1</a></li>
       <li><a href = "/videos/day2.php" target = "main_page">Day2</a></li>
       <li><a href = "/videos/day3.php" target = "main_page">Day3</a></li>
-      <li><a href = "/videos/day4.php" target = "main_page">Day4</a></li>
-      <li><a href = "/videos/day5a.php" target = "main_page">day5 part 1</a></li>
-      <li><a href = "/videos/day5b.php" target = "main_page">Day5 part 2</a></li>
-      <li><a href = "/videos/day5c.php" target = "main_page">Day5 part 3</a></li>
 </ul>
 </li>
 
@@ -40,12 +37,6 @@ require_once "v_s.php";
 <ul class="nested">
      <li> <a href = "assig/assig_1.php" target = "main_page">Assignment1</a><br></li>
      <li> <a href = "assig/assig_2.php" target = "main_page">Assignment2</a><br></li>
-      <li> <a href = "assig/assig_3.php" target = "main_page">Assignment3</a><br></li>
-      <li> <a href = "assig/assig_4.php" target = "main_page">Assignment4</a><br></li>
-      <li> <a href = "assig/assig_5.php" target = "main_page">Assignment5</a><br></li>
-      <li> <a href = "assig/assig_6.php" target = "main_page">Assignment6</a><br></li>
-      <li> <a href = "assig/assig_7.php" target = "main_page">Assignment7</a><br></li>
-</ul>
 </li>
 
 <ul id="myUL">
