97b0e2c318457055e9ddcee47a72c9a806842914
abhishek105-sudo <71555873+abhishek105-sudo@users.noreply.github.com>
2020-11-01 18:15:50 +0530

    Update menu.php

--- page/menu.php
+++ page/menu.php
@@ -20,7 +20,7 @@ require_once "v_s.php";
 </li>
   
    <ul id="myUL">
-  <li><span class="caret"><font color=#851506>Videos</span>
+  <li><span class="caret"><font color=white>Videos</span>
     <ul class="nested">
       <li><a href = "/videos/day1.php" target = "main_page">Day1</a></li>
       <li><a href = "/videos/day2.php" target = "main_page">Day2</a></li>
@@ -32,7 +32,7 @@ require_once "v_s.php";
 </ul>
 </li>
 
-<li><span class="caret"><font color=#851506>Assignments</span>
+<li><span class="caret"><font color=white>Assignments</span>
 <ul class="nested">
      <li> <a href = "assig/assig_1.php" target = "main_page">Assignment1</a><br></li>
      <li> <a href = "assig/assig_2.php" target = "main_page">Assignment2</a><br></li>
@@ -45,7 +45,7 @@ require_once "v_s.php";
 </li>
 
 <ul id="myUL">
-  <li><span class="caret"><font color=#851506>Server OS</span>
+  <li><span class="caret"><font color=white>Server OS</span>
     <ul class="nested">
       <li><a href = "http://software.americaniche.com" target = "main_page">Download</a></li>
 
@@ -53,7 +53,7 @@ require_once "v_s.php";
 </li>
 <ul id="myUL">
   <li><span class="caret">Imprt. source</span>
-    <ul class="nested"><font color=#851506>
+    <ul class="nested"><font color=white>
       <li><a href = "/pdf/ascii.pdf" target = "main_page">ASCII Code</a></li>
       <li><a href = "/pdf/blc.pdf" target = "main_page">Bacis linux command</a></li>
     </ul>
